bb030046384f153e891542e75c2944c619cc699a
gonzika <hziben@gmail.com>
2024-01-07 14:58:56 +0100

    add cicd

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+    resolve: {
+      alias: {
+        src: "/src",
+      },
+    },
+  };
+
+  if (command !== "serve") {
+    config.base = "/threejs-vite/";
+  }
+
+  return config;
+});
